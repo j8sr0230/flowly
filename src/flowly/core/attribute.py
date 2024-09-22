28a82853426ccddcec6c824ccffe7b91c3384c51
j8sr0230 <ronny.scharf08@gmail.com>
2024-09-22 23:42:21 +0200

    Adds attribute.py

---
+++ src/flowly/core/attribute.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+# ***************************************************************************
+# *   Copyright (c) 2024 Ronny Scharf-W. <ronny.scharf08@gmail.com>         *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Library General Public License for more details.                  *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with this program; if not, write to the Free Software   *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************
+
+from uuid import UUID
+from uuid import uuid1
+import logging
+
+
+class Attribute:
+    def __init__(self, name: str, uuid: str = "") -> None:
+        self._name: str = name
+
+        if not uuid:
+            self._uuid: UUID = uuid1()
+        else:
+            try:
+                self._uuid: UUID = UUID(uuid)
+            except ValueError as e:
+                logging.error(f"Invalid UUID provided: {e}")
+                self._uuid: UUID = uuid1()
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, value: str) -> None:
+        self._name: str = value
+
+    @property
+    def uuid(self) -> UUID:
+        return self._uuid
+
+    def __str__(self) -> str:
+        return f"{type(self).__name__}(name={self._name}, uuid={self._uuid})"
+
+    def __repr__(self) -> str:
+        return f"<{type(self).__module__}.{type(self).__name__} {self._uuid} at {hex(id(self))}>"
--- src/flowly/core/main.py
+++ src/flowly/core/main.py
@@ -1,27 +1,36 @@
 #!/usr/bin/env python
 
 # -*- coding: utf-8 -*-
-# ***************************************************************************
-# *   Copyright (c) 2024 Ronny Scharf-W. <ronny.scharf08@gmail.com>         *
-# *                                                                         *
-# *   This program is free software; you can redistribute it and/or modify  *
-# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
-# *   as published by the Free Software Foundation; either version 2 of     *
-# *   the License, or (at your option) any later version.                   *
-# *   for detail see the LICENCE text file.                                 *
-# *                                                                         *
-# *   This program is distributed in the hope that it will be useful,       *
-# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-# *   GNU Library General Public License for more details.                  *
-# *                                                                         *
-# *   You should have received a copy of the GNU Library General Public     *
-# *   License along with this program; if not, write to the Free Software   *
-# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
-# *   USA                                                                   *
-# *                                                                         *
-# ***************************************************************************
+# ************************************************************************
+# * Copyright (c) 2024 Ronny Scharf-W. <ronny.scharf08@gmail.com>        *
+# *                                                                      *
+# * This program is free software; you can redistribute it and/or modify *
+# * it under the terms of the GNU Lesser General Public License (LGPL)   *
+# * as published by the Free Software Foundation; either version 2 of    *
+# * the License, or (at your option) any later version.                  *
+# * for detail see the LICENSE text file.                                *
+# *                                                                      *
+# * This program is distributed in the hope that it will be useful,      *
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the         *
+# * GNU Library General Public License for more details.                 *
+# *                                                                      *
+# * You should have received a copy of the GNU Library General Public    *
+# * License along with this program; if not, write to the Free Software  *
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  *
+# * USA                                                                  *
+# *                                                                      *
+# ************************************************************************
+
+
+from attribute import Attribute
 
 
 if __name__ == "__main__":
-    print("Welcome to flowly")
+    print("Welcome to flowly 0.0.1")
+
+    attrib: Attribute = Attribute("A", uuid="947f4267-792a-11ef-945a-c8d9d2836200")
+    print(attrib)
+
+    attrib.name = "B"
+    print(attrib)
